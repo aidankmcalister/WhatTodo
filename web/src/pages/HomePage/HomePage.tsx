a67b38362618723ddd4ac39e4861e519d85467ab
Aidan <aidankmcalister@gmail.com>
2024-04-15 00:04:02 -0500

    auth added

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -3,18 +3,23 @@ import { Metadata } from '@redwoodjs/web'
 import { useAuth } from 'src/auth'
 
 const HomePage = () => {
-  const { isAuthenticated, userMetadata, currentUser, signUp } = useAuth()
-
-  const handleClick = () => {
-    console.log(userMetadata)
-  }
+  const { isAuthenticated, userMetadata, currentUser, signUp, logIn, logOut } =
+    useAuth()
 
   return (
     <>
       <Metadata title="Home" description="Home page" />
       <header className="flex items-center space-x-3 border-b p-5 shadow-sm">
-        <p>{JSON.stringify({ isAuthenticated })}</p>
-        {/* <img src={currentUser.profile} alt={currentUser.name} /> */}
+        {isAuthenticated && (
+          <div className="flex items-center space-x-3">
+            <img
+              className="h-8 rounded-full"
+              src={currentUser.picture}
+              alt={currentUser.name}
+            />
+            <p>{currentUser.name}</p>
+          </div>
+        )}
         <button
           className="cursor-pointer rounded-md border px-2 py-1 shadow-sm hover:bg-gray-200"
           onClick={signUp}
@@ -23,18 +28,18 @@ const HomePage = () => {
         </button>
         <button
           className="cursor-pointer rounded-md border px-2 py-1 shadow-sm hover:bg-gray-200"
-          onClick={handleClick}
+          onClick={logIn}
         >
-          Test Query
+          Log In
+        </button>
+        <button
+          className="cursor-pointer rounded-md border px-2 py-1 shadow-sm hover:bg-gray-200"
+          onClick={logOut}
+        >
+          Log Out
         </button>
       </header>
-      <section className="p-5">
-        User Metadata
-        <p>{JSON.stringify({ userMetadata })}</p>
-        <br />
-        Current User
-        <p>{JSON.stringify({ currentUser })}</p>
-      </section>
+      <section className="p-5"></section>
     </>
   )
 }
