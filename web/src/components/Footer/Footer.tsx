76826d5fe2251b29cd5d1cb033094f521d1236d4
Aidan <aidankmcalister@gmail.com>
2024-04-23 14:44:15 -0500

    hero icons added

--- web/src/components/Footer/Footer.tsx
+++ web/src/components/Footer/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <footer>footer</footer>
+  return (
+    <footer className="flex items-center justify-center space-x-3 border-t p-5 shadow-sm">
+      Footer
+    </footer>
+  )
 }
 
 export default Footer
