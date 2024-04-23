30f7007f85da636f3ff0fee26441844079394e45
Aidan <aidankmcalister@gmail.com>
2024-04-23 14:44:23 -0500

    header styled

--- web/src/components/Header/Header.tsx
+++ web/src/components/Header/Header.tsx
@@ -1,37 +1,47 @@
-import { useAuth } from 'src/auth'
+import { ListBulletIcon } from '@heroicons/react/24/solid'
 
+import { useAuth } from 'src/auth'
 const Header = () => {
   const { isAuthenticated, currentUser, signUp, logIn, logOut } = useAuth()
   return (
-    <header className="flex items-center space-x-3 border-b p-5 shadow-sm">
-      {isAuthenticated && (
-        <div className="flex items-center space-x-3">
-          <img
-            className="h-8 rounded-full"
-            src={currentUser.picture as string}
-            alt={currentUser.name as string}
-          />
-          <p>{currentUser.name as string}</p>
-        </div>
-      )}
-      <button
-        className="cursor-pointer rounded-md border px-2 py-1 shadow-sm hover:bg-gray-200"
-        onClick={signUp}
-      >
-        Sign Up
-      </button>
-      <button
-        className="cursor-pointer rounded-md border px-2 py-1 shadow-sm hover:bg-gray-200"
-        onClick={logIn}
-      >
-        Log In
-      </button>
-      <button
-        className="cursor-pointer rounded-md border px-2 py-1 shadow-sm hover:bg-gray-200"
-        onClick={logOut}
-      >
-        Log Out
-      </button>
+    <header className="flex items-center justify-between space-x-3 border-b p-5 shadow-sm">
+      <ListBulletIcon className="w-10" />
+      <div className="flex items-center justify-between space-x-3">
+        {isAuthenticated && (
+          <div className="flex items-center space-x-3">
+            <img
+              className="h-8 rounded-full"
+              src={currentUser.picture as string}
+              alt={currentUser.name as string}
+            />
+            <p>{currentUser.name as string}</p>
+          </div>
+        )}
+        {!isAuthenticated && (
+          <div className="flex items-center space-x-3">
+            <button
+              className="cursor-pointer rounded-md border px-2 py-1 shadow-sm hover:bg-gray-200"
+              onClick={signUp}
+            >
+              Sign Up
+            </button>
+            <button
+              className="cursor-pointer rounded-md border px-2 py-1 shadow-sm hover:bg-gray-200"
+              onClick={logIn}
+            >
+              Log In
+            </button>
+          </div>
+        )}
+        {isAuthenticated && (
+          <button
+            className="cursor-pointer rounded-md border px-2 py-1 shadow-sm hover:bg-gray-200"
+            onClick={logOut}
+          >
+            Log Out
+          </button>
+        )}
+      </div>
     </header>
   )
 }
