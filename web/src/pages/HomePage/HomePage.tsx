4c188a27c1bb7aaa398adfc6ce9856102b03a838
Aidan <aidankmcalister@gmail.com>
2024-04-23 14:49:53 -0500

    todo cell integrated

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -2,13 +2,16 @@ import { Metadata } from '@redwoodjs/web'
 
 import Footer from 'src/components/Footer/Footer'
 import Header from 'src/components/Header/Header'
+import TodoCell from 'src/components/TodoCell'
 
 const HomePage = () => {
   return (
     <section className="flex min-h-screen flex-col justify-between">
       <Metadata title="Home" description="Home page" />
       <Header />
-      <section className="p-5"></section>
+      <section className="flex flex-grow flex-col items-center p-5">
+        <TodoCell />
+      </section>
       <Footer />
     </section>
   )
