54faf79bc81adbd879ff315eb787f5e8490c8605
Aidan <aidankmcalister@gmail.com>
2024-04-23 15:24:22 -0500

    reset migration

--- api/db/migrations/20240423201652_reset/migration.sql
+++ api/db/migrations/20240423201652_reset/migration.sql
@@ -1,4 +1,17 @@
 -- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "auth0id" TEXT NOT NULL,
+    "name" TEXT,
+    "picture" TEXT,
+    "email" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "TodoItem" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
@@ -9,6 +22,12 @@ CREATE TABLE "TodoItem" (
 
     CONSTRAINT "TodoItem_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_auth0id_key" ON "User"("auth0id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "TodoItem" ADD CONSTRAINT "TodoItem_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
