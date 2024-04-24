a24f596fae15bd0bfbd0335dee178d9095d92e66
Aidan <aidankmcalister@gmail.com>
2024-04-24 12:52:32 -0500

    is authenticated check

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,18 @@
 import { Metadata } from '@redwoodjs/web'
 
+import { useAuth } from 'src/auth'
 import Footer from 'src/components/Footer/Footer'
 import Header from 'src/components/Header/Header'
 import TodoCell from 'src/components/TodoCell'
 
 const HomePage = () => {
+  const { isAuthenticated } = useAuth()
   return (
     <section className="flex min-h-screen flex-col justify-between">
       <Metadata title="Home" description="Home page" />
       <Header />
       <section className="flex flex-grow flex-col items-center p-5">
-        <TodoCell />
+        {isAuthenticated ? <TodoCell /> : 'Test'}
       </section>
       <Footer />
     </section>
