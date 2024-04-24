22b7f5368743c7eac3cab57312b0e74a57d0d761
Aidan <aidankmcalister@gmail.com>
2024-04-24 14:45:42 -0500

    tailwind colors added

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -2,8 +2,14 @@
 module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-red': '#C0091E',
+        'main-white': '#F3F3EB',
+        'main-gray': '#CFD8D7',
+        'main-brown': '#453831',
+      },
+    },
   },
   plugins: [],
 }
-
