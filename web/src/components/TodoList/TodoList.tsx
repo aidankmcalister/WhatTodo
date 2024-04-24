a045034e8761fff3469b4b41fe2353307caba11b
Aidan <aidankmcalister@gmail.com>
2024-04-24 12:39:13 -0500

    list w style

--- web/src/components/TodoList/TodoList.tsx
+++ web/src/components/TodoList/TodoList.tsx
@@ -2,7 +2,7 @@ import TodoItem from 'src/components/TodoItem'
 
 const TodoList = ({ todoItems }) => {
   return (
-    <ul className="w-64 space-y-3">
+    <ul className="min-w-[90vw] space-y-3 md:min-w-[60vw] xl:min-w-[40vw]">
       {todoItems.map((item) => (
         <TodoItem key={item.id} item={item} />
       ))}
