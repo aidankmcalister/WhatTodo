5e2b8e116915eccbd17a98d6e727b0dc8b361079
Aidan <aidankmcalister@gmail.com>
2024-04-26 17:13:05 -0500

    import change

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,16 @@
 import { Metadata } from '@redwoodjs/web'
 
-import CtaSection from 'src/components/CTASection/CtaSection'
-import FeaturesSection from 'src/components/FeaturesSection/FeaturesSection'
-import HeroSection from 'src/components/HeroSection/HeroSection'
-import TestimonialSection from 'src/components/TestimonialSection/TestimonialSection'
+import CtaSection from 'src/components/CTASection'
+import FeaturesSection from 'src/components/FeaturesSection'
+import HeroSection from 'src/components/HeroSection'
+import TestimonialSection from 'src/components/TestimonialSection'
 
 const HomePage = () => {
   return (
     <>
       <Metadata title="Home" description="Home page" />
 
-      <section className="flex flex-grow flex-col items-center md:w-[85%]">
+      <section className="flex flex-grow flex-col items-center space-y-10 md:w-[85%]">
         <HeroSection />
         <FeaturesSection />
         <TestimonialSection />
