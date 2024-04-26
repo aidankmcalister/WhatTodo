cddab31165b982187805553b3a9ae89852c06cc9
Aidan <aidankmcalister@gmail.com>
2024-04-26 16:57:37 -0500

    testimonial section addeed

---
+++ web/src/components/TestimonialSection/TestimonialSection.tsx
@@ -0,0 +1,14 @@
+const TestimonialSection = () => {
+  return (
+    <div>
+      <h2>{'TestimonialSection'}</h2>
+      <p>
+        {
+          'Find me in ./web/src/components/TestimonialSection/TestimonialSection.tsx'
+        }
+      </p>
+    </div>
+  )
+}
+
+export default TestimonialSection
