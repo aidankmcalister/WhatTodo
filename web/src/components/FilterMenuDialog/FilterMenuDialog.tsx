871dc10e01011b41f4b74804e055437d8ee2152f
Aidan <aidankmcalister@gmail.com>
2024-04-26 00:31:36 -0500

    filter dialog created

---
+++ web/src/components/FilterMenuDialog/FilterMenuDialog.tsx
@@ -0,0 +1,14 @@
+const FilterMenuDialog = () => {
+  return (
+    <div>
+      <h2>{'FilterMenuDialog'}</h2>
+      <p>
+        {
+          'Find me in ./web/src/components/FilterMenuDialog/FilterMenuDialog.tsx'
+        }
+      </p>
+    </div>
+  )
+}
+
+export default FilterMenuDialog
