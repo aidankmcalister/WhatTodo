bdda1f443fa8250aee06acb5475bbdc5a9c6afa7
Aidan <aidankmcalister@gmail.com>
2024-04-26 16:45:31 -0500

    features section created

---
+++ web/src/components/FeaturesSection/FeaturesSection.tsx
@@ -0,0 +1,12 @@
+const FeaturesSection = () => {
+  return (
+    <div>
+      <h2>{'FeaturesSection'}</h2>
+      <p>
+        {'Find me in ./web/src/components/FeaturesSection/FeaturesSection.tsx'}
+      </p>
+    </div>
+  )
+}
+
+export default FeaturesSection
