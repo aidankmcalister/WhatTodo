4f1e3cc21aede0b17965ef3281808df6e94730a6
Aidan <aidankmcalister@gmail.com>
2024-04-26 17:29:11 -0500

    features complete

--- web/src/components/FeaturesSection/FeaturesSection.tsx
+++ web/src/components/FeaturesSection/FeaturesSection.tsx
@@ -11,18 +11,20 @@ const FeaturesSection = () => {
   return (
     <section className="2-full flex flex-col items-center justify-center space-y-3">
       <h1 className="text-2xl font-bold">Features</h1>
-      <Feature
-        title="Easy Task Management"
-        description="Quickly add, complete, and delete tasks with just a few clicks."
-      />
-      <Feature
-        title="User-Friendly Interface"
-        description="Intuitive design that makes navigating and organizing tasks a breeze."
-      />
-      <Feature
-        title="Customizable Categories"
-        description="Organize your tasks into customizable categories for better organization."
-      />
+      <div className="space-y-3  lg:columns-3 lg:space-y-0 ">
+        <Feature
+          title="Easy Task Management"
+          description="Quickly add, complete, and delete tasks with just a few clicks."
+        />
+        <Feature
+          title="User-Friendly Interface"
+          description="Intuitive design that makes navigating and organizing tasks a breeze."
+        />
+        <Feature
+          title="Customizable Categories"
+          description="Organize your tasks into customizable categories for better organization."
+        />
+      </div>
     </section>
   )
 }
