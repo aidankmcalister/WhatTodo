e0d0aee3cbc4eb5e3d01a3cce743d6ee48532817
Aidan <aidankmcalister@gmail.com>
2024-04-26 00:34:27 -0500

    filter dialog setup

--- web/src/components/FilterMenuDialog/FilterMenuDialog.tsx
+++ web/src/components/FilterMenuDialog/FilterMenuDialog.tsx
@@ -1,13 +1,56 @@
-const FilterMenuDialog = () => {
+import { Fragment } from 'react'
+
+import { Dialog, Transition } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/16/solid'
+
+const FilterMenuDialog = ({ open, setOpen }) => {
   return (
-    <div>
-      <h2>{'FilterMenuDialog'}</h2>
-      <p>
-        {
-          'Find me in ./web/src/components/FilterMenuDialog/FilterMenuDialog.tsx'
-        }
-      </p>
-    </div>
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6 dark:bg-gray-800">
+                <button
+                  className="absolute right-5 top-5 hidden rounded-full p-1 hover:bg-gray-200 md:block dark:text-white dark:hover:bg-gray-700"
+                  onClick={() => setOpen(false)}
+                >
+                  <XMarkIcon className="w-6" />
+                </button>
+                <div className="text-center">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-gray-90 mb-5 text-base font-semibold leading-6 dark:text-white"
+                  >
+                    Filters
+                  </Dialog.Title>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
   )
 }
 
