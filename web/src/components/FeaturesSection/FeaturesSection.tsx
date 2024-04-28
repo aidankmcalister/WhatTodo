71f9ac32b2f773bd00812149597809419193dfee
Aidan <aidankmcalister@gmail.com>
2024-04-27 22:56:56 -0500

    remove feature text

--- web/src/components/FeaturesSection/FeaturesSection.tsx
+++ web/src/components/FeaturesSection/FeaturesSection.tsx
@@ -1,7 +1,9 @@
 const Feature = ({ title, description }) => {
   return (
-    <div className="h-32 rounded-md bg-neutral-100 dark:bg-gray-800 p-5">
-      <h1 className="mb-3 text-xl font-bold text-gray-800 dark:text-gray-300">{title}</h1>
+    <div className="h-32 rounded-md bg-neutral-100 p-5 dark:bg-gray-800">
+      <h1 className="mb-3 text-xl font-bold text-gray-800 dark:text-gray-300">
+        {title}
+      </h1>
       <p className="text-gray-800 dark:text-gray-400">{description}</p>
     </div>
   )
@@ -11,7 +13,7 @@ const FeaturesSection = () => {
   return (
     <section className="2-full flex flex-col items-center justify-center space-y-3">
       <h1 className="text-2xl font-bold dark:text-gray-200">Features</h1>
-      <div className="space-y-3  lg:columns-3 lg:space-y-0 ">
+      <div className="space-y-3  lg:columns-2 lg:space-y-0 ">
         <Feature
           title="Easy Task Management"
           description="Quickly add, complete, and delete tasks with just a few clicks."
@@ -20,10 +22,10 @@ const FeaturesSection = () => {
           title="User-Friendly Interface"
           description="Intuitive design that makes navigating and organizing tasks a breeze."
         />
-        <Feature
+        {/* <Feature
           title="Customizable Categories"
           description="Organize your tasks into customizable categories for better organization."
-        />
+        /> */}
       </div>
     </section>
   )
